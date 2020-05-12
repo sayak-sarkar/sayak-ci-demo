459b5a6b12785e1d1149e350dde5df1ee5bfed95
Sayak Sarkar <sayak.bugsmith@gmail.com>
2020-05-13 02:14:14 +0530

    Removed comment to test PR

--- index.test.js
+++ index.test.js
@@ -4,7 +4,6 @@ test('that weekNum returns a value', () => {
   expect(weekNum).not.toBeNull();
 });
 
-// new test added
 test('that weekNum returns a number', () => {
   expect(weekNum).toBeDefined();
 });
